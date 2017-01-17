18e92dfd3fc8981aff3b07858f14838428e10408
John ZuHone <jzuhone@gmail.com>
2017-01-17 11:48:20 -0500

    Tests are now based on a template that allows different options for inputs

--- dea_check/tests/conftest.py
+++ dea_check/tests/conftest.py
@@ -3,13 +3,7 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--generate_answers", 
         help="Generate new answers, but don't test. Argument is the directory to store the answers to.")
-    parser.addoption("--cmd-states-db",
-        help="Commanded states database server (sybase|sqlite)", default="sybase")
 
 @pytest.fixture()
 def generate_answers(request):
     return request.config.getoption('--generate_answers')
-
-@pytest.fixture()
-def cmd_states_db(request):
-    return request.config.getoption("--cmd-states-db")
--- dea_check/tests/test_dea.py
+++ dea_check/tests/test_dea.py
@@ -6,14 +6,20 @@ from ..dea_check import dea_check, model_path
 from acis_thermal_check.test_utils import run_answer_test, \
     run_image_test, run_model
 
-def test_dea(generate_answers, cmd_states_db):
+def dea_test_template(generate_answers, run_start, oflsdir, 
+                      cmd_states_db='sybase'):
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
     model_spec = os.path.join(model_path, "dea_model_spec.json")
-    out_dir = run_model("dea", dea_check, model_spec, cmd_states_db)
+    out_dir = run_model("dea", dea_check, model_spec, run_start, 
+                        oflsdir, cmd_states_db)
     run_answer_test("dea", out_dir, generate_answers)
     run_image_test("1deamzt", "dea", out_dir, generate_answers)
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
+def test_dea_may3016(generate_answers):
+    run_start = "2016:122:12:00:00.000"
+    oflsdir = "/data/acis/LoadReviews/2016/MAY3016/ofls"
+    dea_test_template(generate_answers, run_start, oflsdir)
