2529c65d1739bfa0b75f6f2ad85b6b8703fc7e15
John ZuHone <jzuhone@gmail.com>
2018-01-12 16:09:02 -0500

    This needs to be added to the signature now

--- dea_check/dea_check.py
+++ dea_check/dea_check.py
@@ -36,7 +36,8 @@ VALIDATION_LIMITS = {'1DEAMZT': [(1, 2.0), (50, 1.0), (99, 2.0)],
                      }
 HIST_LIMIT = [20.]
 
-def calc_model(model_spec, states, start, stop, T_dea=None, T_dea_times=None):
+def calc_model(model_spec, states, start, stop, T_dea=None, T_dea_times=None,
+               dh_heater=None, dh_heater_times=None):
     model = xija.ThermalModel('dea', start=start, stop=stop,
                               model_spec=model_spec)
     times = np.array([states['tstart'], states['tstop']])
