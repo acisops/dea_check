5864dacf8781a3644000971eac2dbbcc77f994a4
John ZuHone <jzuhone@gmail.com>
2018-01-26 13:04:24 -0500

    Test loads script

--- dea_check/tests/test_dea.py
+++ dea_check/tests/test_dea.py
@@ -1,12 +1,11 @@
 from ..dea_check import dea_check, model_path
 from acis_thermal_check.regression_testing import \
-    load_test_template
+    load_test_template, test_loads
 import os
 
 default_model_spec = os.path.join(model_path, "dea_model_spec.json")
 
-def test_dea_may3016(answer_store):
-    run_start = "2016:122:12:00:00.000"
-    load_week = "MAY3016"
-    load_test_template("1deamzt", "dea", answer_store, run_start, 
-                       load_week, default_model_spec, dea_check)
+def test_dea_loads(answer_store):
+    for load_week in test_loads:
+        load_test_template("dea", dea_check, default_model_spec,
+                           load_week, answer_store)
