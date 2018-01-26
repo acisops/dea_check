7ee1a2332be1c264a151f3b918c38d2d1aa0ba15
John ZuHone <jzuhone@gmail.com>
2018-01-26 14:14:25 -0500

    Bring this up to date with current code

--- dea_check/tests/test_dea.py
+++ dea_check/tests/test_dea.py
@@ -1,4 +1,5 @@
-from ..dea_check import dea_check, model_path
+from ..dea_check import MSID, VALIDATION_LIMITS, \
+    HIST_LIMIT, calc_model, model_path
 from acis_thermal_check.regression_testing import \
     load_test_template, test_loads
 import os
@@ -7,5 +8,6 @@ default_model_spec = os.path.join(model_path, "dea_model_spec.json")
 
 def test_dea_loads(answer_store):
     for load_week in test_loads:
-        load_test_template("dea", dea_check, default_model_spec,
-                           load_week, answer_store)
+        load_test_template("dea", "1deamzt", load_week,
+                           [MSID, VALIDATION_LIMITS, HIST_LIMIT, calc_model],
+                           answer_store)
