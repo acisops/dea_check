a9ce332fdb044cab967516f778a2fcaa5408091d
John ZuHone <jzuhone@gmail.com>
2018-01-29 17:56:39 -0500

    We're using acis_thermal_check for all of this now

--- dea_check/tests/conftest.py
+++ dea_check/tests/conftest.py
@@ -1,9 +1,2 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption("--answer_store", 
-        help="Generate new answers, but don't test. Argument is the directory to store the answers to.")
-
-@pytest.fixture()
-def answer_store(request):
-    return request.config.getoption('--answer_store')
+from acis_thermal_check.regression_testing import \
+    pytest_addoption, answer_store
--- dea_check/tests/test_dea.py
+++ dea_check/tests/test_dea.py
@@ -1,23 +1,10 @@
 from ..dea_check import VALIDATION_LIMITS, \
     HIST_LIMIT, calc_model, model_path
 from acis_thermal_check.regression_testing import \
-    load_test_template, normal_loads, too_loads, \
-    stop_loads
-import os
-
-default_model_spec = os.path.join(model_path, "dea_model_spec.json")
+    run_test_arrays
 
 def test_dea_loads(answer_store):
-    for load_week in normal_loads:
-        load_test_template("1deamzt", "dea", default_model_spec, load_week,
-                           [VALIDATION_LIMITS, HIST_LIMIT, calc_model],
-                           answer_store, exclude_images=["1deamzt_valid.png","1deamzt.png","pow_sim.png"])
-    for load_week in too_loads:
-        load_test_template("1deamzt", "dea", default_model_spec, load_week,
-                           [VALIDATION_LIMITS, HIST_LIMIT, calc_model],
-                           answer_store, interrupt=True, exclude_images=["1deamzt_valid.png","1deamzt.png","pow_sim.png"])
-    for load_week in stop_loads:
-        load_test_template("1deamzt", "dea", default_model_spec, load_week,
-                           [VALIDATION_LIMITS, HIST_LIMIT, calc_model],
-                           answer_store, interrupt=True, exclude_images=["1deamzt_valid.png","1deamzt.png","pow_sim.png"])
-
+    run_test_arrays("1deamzt", "dea", model_path,
+                    [VALIDATION_LIMITS, HIST_LIMIT, calc_model],
+                    answer_store, 
+                    exclude_images=["1deamzt_valid.png","1deamzt.png","pow_sim.png"])
