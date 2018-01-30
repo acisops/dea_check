544176b6f1dc943852e62f7ee18703600e7a7cae
John ZuHone <jzuhone@gmail.com>
2018-01-30 13:02:34 -0500

    Remove excludes

--- dea_check/tests/test_dea.py
+++ dea_check/tests/test_dea.py
@@ -6,5 +6,4 @@ from acis_thermal_check.regression_testing import \
 def test_dea_loads(answer_store):
     run_test_arrays("1deamzt", "dea", model_path,
                     [VALIDATION_LIMITS, HIST_LIMIT, calc_model],
-                    answer_store, 
-                    exclude_images=["1deamzt_valid.png","1deamzt.png","pow_sim.png"])
+                    answer_store)
