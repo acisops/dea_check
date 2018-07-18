42c21e29ec9289f7546caddff31e8b1e02c07014
John ZuHone <jzuhone@gmail.com>
2018-07-17 22:02:42 -0400

    Bump version

--- dea_check/__init__.py
+++ dea_check/__init__.py
@@ -1,11 +1,12 @@
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 from .dea_check import \
     calc_model
 
+
 def test(*args, **kwargs):
-    '''
+    """
     Run py.test unit tests.
-    '''
+    """
     import testr
     return testr.test(*args, **kwargs)
